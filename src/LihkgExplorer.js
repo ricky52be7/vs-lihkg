2931923aae9ac2d93c2a1a279b09d54084dc7a18
Ricky Yung <yungchunkan@gmail.com>
2019-10-30 23:04:38 +0800

    show Topic

--- src/LihkgExplorer.js
+++ src/LihkgExplorer.js
@@ -1,12 +1,12 @@
 const vscode = require('vscode');
-const { CategoryTreeDataProvider } = require('./providers/CategoryTreeDataProvider');
+const { LihkgTreeDataProvider } = require('./providers/TreeDataProvider');
 
 class LihkgExplorer {
     constructor(context) {
         console.log("Creating LihkgExplorer");
         this.context = context;
         this.viewer = vscode.window.createTreeView('lihkg-view', {
-            treeDataProvider: new CategoryTreeDataProvider()
+            treeDataProvider: new LihkgTreeDataProvider()
         });   
     }
 }
--- src/models/TreeItem.js
+++ src/models/TreeItem.js
@@ -1,12 +1,19 @@
 const vscode = require('vscode');
 
-
 class Category extends vscode.TreeItem {
+    constructor(label, collapsibleState, id) {
+        super(label, collapsibleState);
+        this.id = id;
+    }
+}
+
+class Topic extends vscode.TreeItem {
     constructor(label, collapsibleState) {
         super(label, collapsibleState);
     }
 }
 
 module.exports = {
-    Category
+    Category,
+    Topic
 }
