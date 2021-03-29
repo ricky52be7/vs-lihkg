ab701758dc8acae4089adc58cfdf919d75d21e3a
LLLLinda <lindacheung512@gmail.com>
2021-03-29 13:54:44 +0800

    update to display relative time than abs time

--- src/lihkg.js
+++ src/lihkg.js
@@ -21,9 +21,28 @@ const style = `
 }
 </style>
 `
-
 function formatDate(replayTime) {
-    return new Date(Number(replayTime) * 1000).toString();
+    const now = new Date().getTime()
+    const postDate = Number(replayTime) * 1000
+    const passMillisec = now - postDate;
+    const { interval, scale } = toDynamicDate(passMillisec);
+
+    return `${Math.floor(interval)} ${scale}${interval > 1 ? "s" : ""} ago`
+}
+
+function toDynamicDate(millisecond) {
+    if ((millisecond /= 1e3) > 1 && millisecond < 60)
+        return { interval: millisecond, scale: "second" };
+    if ((millisecond /= 60) > 1 && millisecond < 60)
+        return { interval: millisecond, scale: "minute" };
+    if ((millisecond /= 60) > 1 && millisecond < 24)
+        return { interval: millisecond, scale: "hour" };
+    if ((millisecond /= 24) > 1 && millisecond < 30)
+        return { interval: millisecond, scale: "day" };
+    if ((millisecond /= 30) > 1 && millisecond < 365 / 30)
+        return { interval: millisecond, scale: "month" };
+    if ((millisecond = millisecond * 30 / 365) > 1)
+        return { interval: millisecond, scale: "year" };
 }
 
 /** @param {import("lihkg-api/dist/model").ContentJSON} thread */
